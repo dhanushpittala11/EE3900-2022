bd1c1d904720e1dde97215df7ee22659f1c5e513
dhanushpittala11 <108401638+dhanushpittala11@users.noreply.github.com>
2022-09-25 08:44:19 +0530

    Update Cancel_noise.py

--- filter/codes/Cancel_noise.py
+++ filter/codes/Cancel_noise.py
@@ -2,7 +2,7 @@ import soundfile as sf
 from scipy import signal
 
 #read .wav file 
-input_signal,fs = sf.read('Sound_Noise.wav') 
+input_signal,fs = sf.read('filter_codes_Sound_Noise.wav') 
 
 #sampling frequency of Input signal
 sampl_freq=fs
